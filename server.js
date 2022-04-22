b80abb281e38e4861a4a7c4e9bd83688588f07db
Felipe Navas <felipefnavas@gmail.com>
2022-04-21 23:08:30 -0300

    refactor: using express to listen a port

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-const htpp = require("http");
+import app from "./src/app.js";
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-const rotas = {
-  "/": "Curso de NodeJs",
-  "/livros": "Entrei na pag de livros",
-  "/autores": "Listagem de autores",
-};
-
-const server = htpp.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end(rotas[req.url]);
-});
-
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Servidor escutando em http://localhost:${port}`);
 });
